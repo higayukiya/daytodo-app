e5b0653731984828863a5de9aa00952d1ce9cc78
higayukiya <higa.yukiya@gmail.com>
2021-01-16 16:29:33 +0900

    devise変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
-  get 'home/index'
-  get 'home/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
