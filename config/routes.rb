ef55f34fe9523fe88940efaf7e07b9d80ca77eea
higayukiya <higa.yukiya@gmail.com>
2021-01-23 16:59:11 +0900

    Boardモデル作成とnew作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  get 'home/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'articles#index'
+
+  resources :articles, only: [:show, :new, :create]
 end
